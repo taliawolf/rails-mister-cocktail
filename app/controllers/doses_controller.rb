72a2ba637481126a1f75eea15049f32df59f99d9
talia wolf <taliamwolf@gmail.com>
2020-02-12 14:42:34 +0200

    cocktails controller done and dose model and dose controller and dose view

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,26 @@
+class DosesController < ApplicationController
+
+  def new
+    @dose = Dose.new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+  end
+
+  def create
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new(dose_params)
+    @dose.cocktail = @cocktail
+    @dose.save
+    redirect_to cocktail_path(@cocktail)
+  end
+
+  def destroy
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient, :cocktail)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts 'cleaning database'
+Ingredient.destroy_all
+
+puts "getting API"
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+response = HTTParty.get(url)
+data = response.parsed_response
+puts "API has been parsed"
+list_of_ingredients = data['drinks']
+list_of_ingredients.each do |ingredient|
+  new_ingredient = Ingredient.new(name: ingredient["strIngredient1"])
+  new_ingredient.save!
+end
+puts "you now have #{Ingredient.count} ingredients"
