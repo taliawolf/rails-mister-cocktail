4b62d336fd16d4a255bb93926b96c205a5038af6
talia wolf <taliamwolf@gmail.com>
2020-02-12 11:37:54 +0200

    all models done

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,4 +1,8 @@
 class Dose < ApplicationRecord
   belongs_to :ingredient
   belongs_to :cocktail
+  validates :description, presence: true
+  validates :cocktail, presence: true
+  validates :ingredient, presence: true
+  validates_uniqueness_of :cocktail, scope: :ingredient
 end
