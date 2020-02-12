018721393374b3cbb1b5b1db3dec837105909cd3
talia wolf <taliamwolf@gmail.com>
2020-02-12 15:54:39 +0200

    backend is done

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -9,6 +9,7 @@ class DosesController < ApplicationController
     @cocktail = Cocktail.find(params[:cocktail_id])
     @dose = Dose.new(dose_params)
     @dose.cocktail = @cocktail
+    @dose.ingredient = Ingredient.find(params[:dose][:ingredient_id])
     @dose.save
     redirect_to cocktail_path(@cocktail)
   end
@@ -16,11 +17,12 @@ class DosesController < ApplicationController
   def destroy
     @dose = Dose.find(params[:id])
     @dose.destroy
+    redirect_to cocktail_path(@dose.cocktail)
   end
 
   private
 
   def dose_params
-    params.require(:dose).permit(:description, :ingredient, :cocktail)
+    params.require(:dose).permit(:description, :ingredient_id)
   end
 end
