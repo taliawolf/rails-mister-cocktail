facfbead1eb345ec69cc2e1d2e8ed6e1ce77128b
talia wolf <taliamwolf@gmail.com>
2020-02-15 11:59:36 +0200

    destroy action added for cocktails

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -22,6 +22,12 @@ class CocktailsController < ApplicationController
     end
  end
 
+ def destroy
+   @cocktail = Cocktail.find(params[:id])
+   @cocktail.destroy
+   redirect_to cocktails_path, notice: "#{@cocktail.name} was successfully deleted"
+ end
+
   private
 
   def cocktail_params
