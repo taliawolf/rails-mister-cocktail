32e62b9ba419749f58d9ee2391654620f6e9ce45
talia wolf <taliamwolf@gmail.com>
2020-03-07 12:25:35 +0200

    not done

--- Gemfile
+++ Gemfile
@@ -37,6 +37,7 @@ gem 'jbuilder', '~> 2.5'
 # gem 'capistrano-rails', group: :development
 gem 'autoprefixer-rails'
 gem 'font-awesome-sass', '~> 5.6.1'
+# gem 'pg_search', '~> 2.3.0'
 gem 'simple_form'
 gem 'httparty'
 
--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -4,4 +4,11 @@ class Cocktail < ApplicationRecord
   has_many :ingredients, through: :doses
   validates :name, uniqueness: true, presence: true
   has_one_attached :photo
+
+  # include PgSearch::Model
+  # pg_search_scope :search_by_name,
+  #   against: [:name],
+  #   using: {
+  #     tsearch: { prefix: true }
+  #   }
 end
